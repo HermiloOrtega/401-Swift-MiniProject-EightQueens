777e17f78f06e7a70e3eaf01d03d448f71cedce8
HermiloOrtega <hermilo.jh@gmail.com>
2020-06-26 11:31:14 -0700

    Init commit

---
+++ 401-Swift-MiniProject-EightQueens/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  401-Swift-MiniProject-EightQueens
+//
+//  Created by Jose Hermilo Ortega Martinez on 2020-06-26.
+//  Copyright © 2020 Jose Hermilo Ortega Martinez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
